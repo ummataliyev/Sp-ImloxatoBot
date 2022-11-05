fbc99e238bf1ff39e26425645a3dc08aa342ca74
ummataliyev <djohnummataliyev@gmail.com>
2022-11-05 18:48:20 +0500

    UzbImloBot

---
+++ checkWord.py
@@ -0,0 +1,19 @@
+from uzwords import words
+from difflib import get_close_matches
+
+def checkWord(word,words=words):
+    word = word.lower()
+    matches = set(get_close_matches(word, words))
+    available = False # bunday so'z mavjud emas
+
+    if word in matches:
+        available = True # mavjud
+        matches = word
+    elif 'ҳ' in word:
+        word = word.replace('ҳ', 'х')
+        matches.update(get_close_matches(word, words))
+    elif 'х' in word:
+        word = word.replace('х', 'ҳ')
+        matches.update(get_close_matches(word, words))
+
+    return {'available': available, 'matches': matches}
